ba1f6a574c7ef2bcd33c73f44d8dc6998d278f47
Joyce He <54854214+jiayu-joyce-he@users.noreply.github.com>
2019-10-21 10:31:15 -0700

    Update js-intermediate.js

--- js-intermediate.js
+++ js-intermediate.js
@@ -39,13 +39,16 @@ console.log(getFib3(10))
 
 //recursion with array
 getFib4 = num => {
-	if(num <= 2 ) {
-		return [1,1]
+  if (num < 0){
+    return "Please input a number equal or larger than 0."
+  } else if(num <= 2 ) {   // the condiitonal will be <=2 since index 3 would be the first value to change
+		return new Array(num).fill(1)
 	} else {
 		fibArray = getFib4(num-1)
-		return [...fibArray, fibArray[num-2] + fibArray[num-3]]
+		return [...fibArray, fibArray[num-2] + fibArray[num-3]] // has to be -2 and -3 given the fibArray expanded is -1. The previous two would be -2 and -3 accordingly.
 	}
 }
+
 console.log(getFib4(10))
 
 
